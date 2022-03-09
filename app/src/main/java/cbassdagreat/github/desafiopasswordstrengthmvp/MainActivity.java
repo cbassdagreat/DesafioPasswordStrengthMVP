97702148c71f762eab4238edb2eb6adc8a5604f5
Cbass <claveria.sebastian@gmail.com>
2022-03-09 20:25:37 -0300

    Initial commit

---
+++ app/src/main/java/cbassdagreat/github/desafiopasswordstrengthmvp/MainActivity.java
@@ -0,0 +1,17 @@
+package cbassdagreat.github.desafiopasswordstrengthmvp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import java.util.Collections;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
